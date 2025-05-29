3102c5e97026f0f0a572f55ab24d334dbadb5449
smshaban117 <smshaban117@gmail.com>
2025-05-29 00:22:09 +0000

    Created funtion to display computer's choice

---
+++ index.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+   let randomNum = Math.random();
+
+ if (randomNum < 0.33)
+ {return "rock";}
+ else if (randomNum < 0.66)
+ {return "paper";}
+ else {return "scissors";}
+}
+
+function getHumanChoice(){
+
+}
