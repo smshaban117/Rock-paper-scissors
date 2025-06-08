51aa6eded619b2c88221666cf73be61a31efc090
smshaban117 <smshaban117@gmail.com>
2025-06-08 15:37:55 +0000

    Finished rock paper scissors project

--- index.js
+++ index.js
@@ -9,5 +9,43 @@ function getComputerChoice() {
 }
 
 function getHumanChoice(){
+  let game = prompt("rock, paper, scissors shoot!");
+  if (game==="rock") {return "rock";}
+  else if (game==="paper") {return "paper";}
+  else {return "scissors";}
 
 }
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+  if (humanChoice == "rock" && computerChoice == "scissors")
+  { humanScore++
+    return "You win, rock beats scissors"}
+  else if (humanChoice == "paper" && computerChoice == "rock")
+  { humanScore++
+    return "You win, paper beats rock "}
+  else if (humanChoice == computerChoice) 
+  { return "It's a draw"}
+  else { computerScore++
+    return "You lose, try again"}
+
+}
+
+function playGame() {
+ console.log (playRound(getHumanChoice(), getComputerChoice()));
+ console.log (playRound(getHumanChoice(), getComputerChoice()));
+ console.log (playRound(getHumanChoice(), getComputerChoice()));
+ console.log (playRound(getHumanChoice(), getComputerChoice()));
+ console.log (playRound(getHumanChoice(), getComputerChoice()));
+
+ console.log(`Final Score - You: ${humanScore}, Computer: ${computerScore}`);
+}
+
+
+console.log(playGame());
+
+
+
