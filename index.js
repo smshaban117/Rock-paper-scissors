94b1e5419820048e28f968b8eb4fb108fc503f21
smshaban117 <smshaban117@gmail.com>
2025-07-24 21:40:05 +0000

    Completed the JS for rock paper scissors

--- index.js
+++ index.js
@@ -21,23 +21,68 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
+  let result=""
   if (humanChoice == "rock" && computerChoice == "scissors")
   { humanScore++
-    return "You win, rock beats scissors"}
+    return result = "You win, rock beats scissors"}
   else if (humanChoice == "paper" && computerChoice == "rock")
   { humanScore++
-    return "You win, paper beats rock "}
+    return result = "You win, paper beats rock "}
   else if (humanChoice == computerChoice) 
-  { return "It's a draw"}
+  { return result = "It's a draw"}
+  else if (humanChoice =="scissors" && computerChoice =="paper" )
+   { humanScore++
+   return result = "You win, scissors beats paper"}
   else { computerScore++
-    return "You lose, try again"}
+    return result = "You lose, try again"}
+
+   
 
 }
 
+function winner(){
+if (humanScore == 5 && computerScore < 5)
+{ return "You are the winner"}
+else if (computerScore == 5 && humanScore < 5)
+{return "You are a loser"}
+  else {return ""}}
+ 
 const rock = document.querySelector("#rock")
 const paper = document.querySelector("#paper")
-const = scissors = document.querySelector("#scissors")
+const scissors = document.querySelector("#scissors")
 
-rock.addEventListener("click", playRound)
-paper.addEventListener("click", playRound)
-scissors.addEventListener("click", playRound)
+const output = document.querySelector("#output")
+
+const output2 = document.querySelector("#output2")
+const output3 = document.querySelector("#output3")
+
+
+rock.addEventListener("click", function()
+{ if (humanScore === 5 || computerScore === 5) return;
+  output.innerHTML += playRound("rock",getComputerChoice()) + "<br>";
+  output2.innerHTML = `Score - Human: ${humanScore} | Computer ${computerScore}`
+   let message = winner()
+   if (message !=="") output3.innerHTML = message
+
+ });
+
+paper.addEventListener("click", function() 
+{
+  if (humanScore === 5 || computerScore === 5) return;
+output.innerHTML +=  playRound("paper", getComputerChoice()) + "<br>";
+output2.innerHTML = `Score - Human: ${humanScore} | Computer ${computerScore}`
+   let message = winner()
+   if (message !=="") output3.innerHTML = message
+ });
+
+scissors.addEventListener("click", function() 
+{if (humanScore === 5 || computerScore === 5) return;
+  output.innerHTML +=  playRound("scissors", getComputerChoice())+ "<br>" ;
+output2.innerHTML = `Score - Human: ${humanScore} | Computer ${computerScore}`
+   let message = winner()
+   if (message !=="") output3.innerHTML = message});
+
+
+
+
+
